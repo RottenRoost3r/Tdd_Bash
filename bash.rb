eea80b4fcbbf7b9efccd0c8a3ef09098161a5f3c
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-25 14:26:14 -0400

    pass, num check

--- bash.rb
+++ bash.rb
@@ -1,3 +1,12 @@
 def number_comparison(myticket, winningticket)
-1
+	myticket = myticket.split('')
+	winningticket = winningticket.split('')
+	matching_numbers = 0
+	winningticket.each_with_index do |num, index|
+		if num == myticket[index]
+			matching_numbers += 1
+		end
+	end
+	matching_numbers
+
 end
